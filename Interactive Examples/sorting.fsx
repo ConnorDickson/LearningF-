7243307e3095082939f849d8da1d09dd17df318c
Connor Dickson <cdickson14@qub.ac.uk>
2018-09-10 20:20:50 +0100

    Sorting

--- Interactive Examples/sorting.fsx
+++ Interactive Examples/sorting.fsx
@@ -16,3 +16,12 @@ let rec quicksort list =
 
 //test
 printfn "%A" (quicksort [1;5;23;18;9;1;3])
+
+let rec quicksort2 = function
+   | [] -> []                         
+   | first::rest -> 
+        let smaller,larger = List.partition ((>=) first) rest 
+        List.concat [quicksort2 smaller; [first]; quicksort2 larger]
+        
+// test code  
+printfn "%A" (quicksort2 [1;5;23;18;9;1;3])
